125f39603e253262415d8b9cbed6fb3a3f00e605
liyang6620 <134552904+liyang6620@users.noreply.github.com>
2024-10-14 22:13:47 +1300

    Update try.py

--- try.py
+++ try.py
@@ -9,6 +9,7 @@ from scipy.interpolate import CubicSpline
 predictions = pd.read_csv('predictions.csv')
 crime_monthly = pd.read_csv('crime_monthly.csv')
 crime_monthly['Date'] = pd.to_datetime(crime_monthly['Date'])
+
 total_sentences_predictions = predictions[predictions['Target'] == 'Total Sentences'].sort_values(['Year','Region'])
 total_sentences_predictions = total_sentences_predictions.loc[total_sentences_predictions.groupby(['Year', 'Region'])['MSE'].idxmin()]
 
@@ -68,6 +69,7 @@ if location == "ALL":
 else:
     crime = st.number_input("Crime", value=0.0, step=0.1)
     selected_df = total_sentences_predictions_monthly[(total_sentences_predictions_monthly['Date'] <= selected_date) & (total_sentences_predictions_monthly['Location'] == location)]
+    selected_df.iloc[-1, selected_df.columns.get_loc('Crime')] = crime
     selected_df['Crime_Rolling_Std_3'] = selected_df['Crime'].rolling(3,1).std()
     selected_df['Crime_Rolling_Std_6'] = selected_df['Crime'].rolling(6,1).std()
 st.write(location_id)
