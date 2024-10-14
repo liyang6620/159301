7606dddbfed8acf55c64231468686e0a93385b21
liyang6620 <134552904+liyang6620@users.noreply.github.com>
2024-10-14 22:30:44 +1300

    Update try.py

--- try.py
+++ try.py
@@ -73,10 +73,13 @@ else:
     selected_df.iloc[-1, selected_df.columns.get_loc('Crime')] = crime
     selected_df['Crime_Rolling_Std_3'] = selected_df['Crime'].rolling(3,1).std()
     selected_df['Crime_Rolling_Std_6'] = selected_df['Crime'].rolling(6,1).std()
+
+if st.button("进行预测"):
     features = selected_df.iloc[-1][['Crime_Rolling_Std_3', 'Crime_Rolling_Std_6']]
     features['Location Id'] = location_id
     dtest = xgb.DMatrix([features])
     predictions = loaded_model.predict(dtest)
+    
 st.write(predictions)
 
 data = {
